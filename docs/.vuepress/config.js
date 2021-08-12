3079e8448b283ed66b87c5522c55bd6904e8c8e8
maqingbo <wmaqingbo@163.com>
2021-08-12 15:53:53 +0800

    feat: 新增线上版本；

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  base: '/fqzyzbzy/',
+  // title: '父权制与资本主义',
+  description: '父权制与资本主义，电子书，上野千鹤子',
+  head: [
+    ['link', { rel: 'icon', href: '/icon.png' }]
+  ],
+  themeConfig: {
+    // displayAllHeaders: true,
+    lastUpdated: '上次更新',
+    sidebarDepth: 2,
+    sidebar: {
+      '/chapters/': [
+        '01.md', '02.md', '03.md',
+        '04.md', '05.md', '06.md',
+        '07.md', '08.md', '09.md',
+        '10.md', '11.md', '12.md',
+        '13.md', '14.md', '15.md',
+        '16.md', '17.md'
+      ]
+    }
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,9 @@
+---
+home: true
+heroImage: /cover.jpg
+heroText: 父权制与资本主义
+tagline: 上野千鹤子
+actionText: 开始阅读
+actionLink: /chapters/01/
+# footer: MIT Licensed | Copyright © 2018-present 上野千鹤子
+---
